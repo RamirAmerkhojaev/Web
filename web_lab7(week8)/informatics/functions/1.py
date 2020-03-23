67e85f7e50d031edf8a43ced35b8f2f802cd6b9a
RamirAmerkhojaev <mr.healer@list.ru>
2020-03-23 22:43:37 +0600

    week 9 lab

--- web_lab7(week8)/informatics/functions/1.py
+++ web_lab7(week8)/informatics/functions/1.py
@@ -1,6 +1,4 @@
 def function(a,b,c,d):
-    min1 = 0
-    min2 = 0
     if a<=b and a<=c and a<=d:
     	print(a)
     elif b<=a and b<=c and b<=d:
---
+++ web_lab8(week9)/week9/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Dell\Desktop\Labki\web_lab8(week9)\week9\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
